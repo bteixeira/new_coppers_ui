ba6332efc3e27e870c7225ec37d1c450816d1508
Bruno Teixeira <bruno.teixeira@locafox.de>
2015-11-16 19:10:17 +0100

    Exploring how to present a table on a small screen

--- js/detailed_table.js
+++ js/detailed_table.js
@@ -1,48 +1,56 @@
 var data = [
     {
-        time: '1944.06.06@06:33',
+        date: '1944.06.06',
+        time: '06:33',
         amount: 342,
         tags: 'bombs',
         account: 'Credit Card'
     },
     {
-        time: '1944.06.06@06:33',
+        date: '1944.06.06',
+        time: '06:33',
         amount: 342,
         tags: 'bombs',
         account: 'Credit Card'
     },
     {
-        time: '1944.06.06@06:33',
+        date: '1944.06.06',
+        time: '06:33',
         amount: 342,
         tags: 'bombs',
         account: 'Credit Card'
     },
     {
-        time: '1944.06.06@06:33',
+        date: '1944.06.06',
+        time: '06:33',
         amount: 342,
         tags: 'bombs',
         account: 'Credit Card'
     },
     {
-        time: '1944.06.06@06:33',
+        date: '1944.06.06',
+        time: '06:33',
         amount: 342,
         tags: 'bombs',
         account: 'Credit Card'
     },
     {
-        time: '1944.06.06@06:33',
+        date: '1944.06.06',
+        time: '06:33',
         amount: 342,
         tags: 'bombs',
         account: 'Credit Card'
     },
     {
-        time: '1944.06.06@06:33',
+        date: '1944.06.06',
+        time: '06:33',
         amount: 342,
         tags: 'bombs',
         account: 'Credit Card'
     },
     {
-        time: '1944.06.06@06:33',
+        date: '1944.06.06',
+        time: '06:33',
         amount: 342,
         tags: 'bombs',
         account: 'Credit Card'
@@ -58,11 +66,11 @@ $(function () {
     data.forEach(function (entry) {
         total += entry.amount;
         var $tr = $('<tr></tr>');
-        $tr.append('<td>' + entry.time + '</td>');
+        $tr.append('<td>' + entry.date + ' @' + entry.time + '</td>');
         $tr.append('<td>' + formatCurrency(entry.amount) + '</td>');
         $tr.append('<td>#' + entry.tags + '</td>');
         $tr.append('<td>' + entry.account + '</td>');
-        $tr.append('<td>' + formatCurrency(total) + '</td>');
+        $tr.append('<td class="col-accumulated">' + formatCurrency(total) + '</td>');
         $tr.append('<td><a href="#">[E]</a> <a href="#">[X]</a></td>');
         $('table tbody').append($tr)
     });
