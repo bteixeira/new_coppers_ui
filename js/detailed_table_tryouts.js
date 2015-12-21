4733f8e69aeeebedff910a579f0dfb50d9abb256
Bruno Teixeira <bteixeira666@gmail.com>
2015-12-21 16:18:07 +0000

    Added real world data

--- js/detailed_table_tryouts.js
+++ js/detailed_table_tryouts.js
@@ -57,8 +57,418 @@ var data = [
     }
 ];
 
+data = [{
+    date: '2010-10-01',
+    time: '00:00',
+    amount: 1.80,
+    tags: 'diario:alimentacao',
+    account: '',
+    description: 'paes com passas, girassol, s.pedro'
+},
+{
+    date: '2010-10-01',
+    time: '00:00',
+    amount: 7.85,
+    tags: 'diario:alimentacao',
+    account: '',
+    description: 'almoco vitaminas, plaza'
+},
+{
+    date: '2010-10-01',
+    time: '00:00',
+    amount: 1.00,
+    tags: 'diario:alimentacao',
+    account: '',
+    description: 'cafe + gelado amoreiras'
+},
+{
+    date: '2010-10-01',
+    time: '00:00',
+    amount: 0.90,
+    tags: 'diario:alimentacao',
+    account: '',
+    description: 'merenda'
+},
+{
+    date: '2010-10-01',
+    time: '00:00',
+    amount: 5.50,
+    tags: 'diario:alimentacao',
+    account: '',
+    description: 'jantar amoreiras'
+},
+{
+    date: '2010-10-01',
+    time: '00:00',
+    amount: 2.70,
+    tags: 'diario:alimentacao',
+    account: '',
+    description: 'gelado amoreiras'
+},
+{
+    date: '2010-10-01',
+    time: '00:00',
+    amount: 2.38,
+    tags: 'diario:alimentacao',
+    account: '',
+    description: 'bolachas pao de acucar amoreiras'
+},
+{
+    date: '2010-10-01',
+    time: '00:00',
+    amount: 11.00,
+    tags: 'saidas:noite',
+    account: '',
+    description: 'entrada arraial IST'
+},
+{
+    date: '2010-10-01',
+    time: '00:00',
+    amount: 10.00,
+    tags: 'saidas:noite',
+    account: '',
+    description: 'bebidas (?)'
+},
+{
+    date: '2010-10-02',
+    time: '00:00',
+    amount: 6.75,
+    tags: 'diario:alimentacao',
+    account: '',
+    description: 'almoco vitaminas cascais shopping'
+},
+{
+    date: '2010-10-02',
+    time: '00:00',
+    amount: 1.00,
+    tags: 'diario:alimentacao',
+    account: '',
+    description: 'garrafa agua'
+},
+{
+    date: '2010-10-02',
+    time: '00:00',
+    amount: 13.80,
+    tags: 'compras',
+    account: '',
+    description: 'conjunto pesos sport zone cascaishopping'
+},
+{
+    date: '2010-10-02',
+    time: '00:00',
+    amount: 20.00,
+    tags: 'gasolina',
+    account: '',
+    description: 'gasolina, BP s.pedro, 14.61lt, 1.369eur/lt'
+},
+{
+    date: '2010-10-02',
+    time: '00:00',
+    amount: 0.60,
+    tags: 'diario:alimentacao',
+    account: '',
+    description: 'cafe bomba BP s.pedro'
+},
+{
+    date: '2010-10-02',
+    time: '00:00',
+    amount: 10.00,
+    tags: 'saidas:noite',
+    account: '',
+    description: 'bebidas bairro alto (?)'
+},
+{
+    date: '2010-10-03',
+    time: '00:00',
+    amount: 0.55,
+    tags: 'saidas',
+    account: '',
+    description: 'portagem A5 sentido lx'
+},
+{
+    date: '2010-10-03',
+    time: '00:00',
+    amount: 12.00,
+    tags: 'saidas',
+    account: '',
+    description: 'jantar solar do duque com o tok'
+},
+{
+    date: '2010-10-03',
+    time: '00:00',
+    amount: 0.55,
+    tags: 'saidas',
+    account: '',
+    description: 'portagem A5 sentido cascais'
+},
+
+{
+    date: '2012-11-01',
+    time: '00:00',
+    amount: 1.28,
+    tags: 'saidas',
+    account: 'ALaCard',
+    description: 'pipocas pingo doce madorna'
+},
+{
+    date: '2012-11-01',
+    time: '00:00',
+    amount: 15.00,
+    tags: 'gasolina',
+    account: 'Cartao',
+    description: 'gasolina repsol sdr'
+},
+{
+    date: '2012-11-02',
+    time: '00:00',
+    amount: 5.50,
+    tags: 'diario:alimentacao',
+    account: 'Cartao',
+    description: 'almoco twin towers'
+},
+{
+    date: '2012-11-02',
+    time: '00:00',
+    amount: 0.30,
+    tags: 'diario:alimentacao',
+    account: 'Cash',
+    description: 'decaf ihru'
+},
+{
+    date: '2012-11-02',
+    time: '00:00',
+    amount: 0.30,
+    tags: 'diario:alimentacao',
+    account: 'Cash',
+    description: 'chocolate quente ihru'
+},
+{
+    date: '2012-11-03',
+    time: '00:00',
+    amount: 0.55,
+    tags: 'outros',
+    account: 'Cartao',
+    description: 'portagem A5 carcavelos'
+},
+{
+    date: '2012-11-04',
+    time: '00:00',
+    amount: 1.80,
+    tags: 'outros',
+    account: 'Cartao',
+    description: 'bilhete CP s.pedro'
+},
+{
+    date: '2012-11-05',
+    time: '00:00',
+    amount: 79.90,
+    tags: 'mensal',
+    account: 'Transferencia',
+    description: 'holmes place'
+},
+{
+    date: '2012-11-05',
+    time: '00:00',
+    amount: 3.95,
+    tags: 'diario:alimentacao',
+    account: 'Cash',
+    description: 'pequeno almoco pastelaria sem stress'
+},
+{
+    date: '2012-11-05',
+    time: '00:00',
+    amount: 0.30,
+    tags: 'diario:alimentacao',
+    account: 'Cash',
+    description: 'cappucino ihru'
+},
+{
+    date: '2012-11-05',
+    time: '00:00',
+    amount: 24.98,
+    tags: 'compras',
+    account: 'Cartao',
+    description: 'cartao SD e hub usb - el corte ingles'
+},
+{
+    date: '2012-11-05',
+    time: '00:00',
+    amount: 7.65,
+    tags: 'diario:alimentacao',
+    account: 'Cartao',
+    description: 'almoco H3 el corte ingles'
+},
+{
+    date: '2012-11-05',
+    time: '00:00',
+    amount: 0.30,
+    tags: 'diario:alimentacao',
+    account: 'Cash',
+    description: 'decaf ihru'
+},
+{
+    date: '2012-11-05',
+    time: '00:00',
+    amount: 3.01,
+    tags: '',
+    account: 'Cartao',
+    description: 'compras: rolo cozinha, chocolate e laranjas'
+},
+{
+    date: '2012-11-06',
+    time: '00:00',
+    amount: 3.35,
+    tags: 'diario:alimentacao',
+    account: 'Cash',
+    description: 'pequeno almoco hospital HPV'
+},
+{
+    date: '2012-11-06',
+    time: '00:00',
+    amount: 2.00,
+    tags: 'saude',
+    account: 'Cartao',
+    description: 'vacinas'
+},
+{
+    date: '2012-11-06',
+    time: '00:00',
+    amount: 12.25,
+    tags: 'diario:alimentacao',
+    account: 'Cartao',
+    description: 'almoco portugalia amoreiras cc'
+},
+{
+    date: '2012-11-06',
+    time: '00:00',
+    amount: 13.00,
+    tags: 'saidas',
+    account: 'Cartao',
+    description: 'jantar pig indiano fishtail'
+},
+{
+    date: '2012-11-06',
+    time: '00:00',
+    amount: 16.46,
+    tags: 'mensal',
+    account: 'Transferencia',
+    description: 'conta gas'
+},
+{
+    date: '2012-11-07',
+    time: '00:00',
+    amount: 1.10,
+    tags: 'compras:alimentacao',
+    account: 'Cash',
+    description: 'pao de leite mercado de arroios'
+},
+{
+    date: '2012-11-07',
+    time: '00:00',
+    amount: 16.75,
+    tags: 'mensal',
+    account: 'Transferencia',
+    description: 'conta agua e luz'
+},
+{
+    date: '2012-11-07',
+    time: '00:00',
+    amount: 7.75,
+    tags: 'diario:alimentacao',
+    account: 'Cartao',
+    description: 'almoco H3 amoreiras plaza'
+},
+{
+    date: '2012-11-07',
+    time: '00:00',
+    amount: 14.90,
+    tags: 'compras',
+    account: 'Cartao',
+    description: 'caixa hdd externo pao de acucar amoreiras'
+},
+{
+    date: '2012-11-07',
+    time: '00:00',
+    amount: 4.90,
+    tags: 'compras:alimentacao',
+    account: 'ALaCard',
+    description: 'compras jantar pingo doce'
+},
+{
+    date: '2012-11-08',
+    time: '00:00',
+    amount: 7.75,
+    tags: 'diario:alimentacao',
+    account: 'Cartao',
+    description: 'almoco H3 amoreiras plaza'
+},
+{
+    date: '2012-11-08',
+    time: '00:00',
+    amount: 0.63,
+    tags: 'diario:alimentacao',
+    account: 'Cash',
+    description: 'decaf amoreiras square'
+},
+{
+    date: '2012-11-08',
+    time: '00:00',
+    amount: 4.68,
+    tags: 'compras:alimentacao',
+    account: 'ALaCard',
+    description: 'compras pingo doce'
+},
+{
+    date: '2012-11-09',
+    time: '00:00',
+    amount: 4.67,
+    tags: 'compras:alimentacao',
+    account: 'Cartao',
+    description: 'compras minipreco'
+},
+{
+    date: '2012-11-09',
+    time: '00:00',
+    amount: 7.90,
+    tags: 'diario:alimentacao',
+    account: 'Cartao',
+    description: 'almoco vitaminas amoreiras cc'
+},
+{
+    date: '2012-11-09',
+    time: '00:00',
+    amount: 1.80,
+    tags: 'diario:alimentacao',
+    account: 'Cash',
+    description: 'cafe e bolo amoreiras cc'
+},
+{
+    date: '2012-11-09',
+    time: '00:00',
+    amount: -14.90,
+    tags: 'compras',
+    account: 'Cash',
+    description: 'devolucao caixa hdd externo jumbo box amoreiras cc'
+},
+{
+    date: '2012-11-09',
+    time: '00:00',
+    amount: 0.65,
+    tags: 'compras',
+    account: 'Cash',
+    description: 'elasticos papelaria amoreiras cc'
+},
+{
+    date: '2012-11-09',
+    time: '00:00',
+    amount: 14.00,
+    tags: 'saidas',
+    account: 'Cartao',
+    description: 'jantar chines not pina'
+}];
+
 function formatCurrency(amount) {
-    return '<span class="nowrap">$ ' + amount.toFixed(2) + '</span>';
+    return '<span class="nowrap">&euro; ' + amount.toFixed(2) + '</span>';
 }
 
 $(function () {
