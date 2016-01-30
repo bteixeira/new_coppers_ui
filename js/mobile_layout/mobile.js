4118cb3c950d4c71ef854196d9c5ea03942b3ee6
Bruno Teixeira <bteixeira666@gmail.com>
2016-01-30 22:07:06 +0100

    Added awesome native CSS transition effect for expanding calendar which probably doesn't work on some browsers.

--- js/mobile_layout/mobile.js
+++ js/mobile_layout/mobile.js
@@ -28,7 +28,35 @@ $(function () {
 
     $('.date-picker .trigger-calendar').on('click', function (ev) {
         ev.preventDefault();
-        $(this).closest('.date-picker').find('.calendar-popup').toggleClass('onscreen');
+
+        var $picker = $(this).closest('.date-picker');
+        var $popup = $picker.find('.calendar-popup');
+        //$popup.transition
+        var h;
+        if ($popup.is('.onscreen')) {
+            h = $popup.outerHeight();
+            $popup.css('height', h + 'px');
+            $popup[0].offsetHeight;
+            $popup.css('transition', 'height linear 150ms');
+            $popup.one('transitionend', function () {
+                $popup.css('height', '');
+                $popup.css('transition', '');
+                $popup.removeClass('onscreen');
+            });
+            $popup.css('height', '0');
+        } else {
+            $popup.toggleClass('onscreen', true);
+            h = $popup.outerHeight();
+            $popup.css('height', '0');
+            $popup[0].offsetHeight;
+            $popup.css('transition', 'height 300ms');
+            $popup.one('transitionend', function () {
+                $popup.css('height', '');
+                $popup.css('transition', '');
+            });
+            $popup.css('height', h + 'px');
+        }
+
     });
     $('.date-picker .calendar-popup tbody td:not(.week-number)').on('click', function (ev) {
         $(this).closest('.date-picker').find('.trigger-calendar .value').text($(this).text());
@@ -36,7 +64,8 @@ $(function () {
     });
 
     $('.date-picker input[type="checkbox"]').on('click', function (ev) {
-        $(this).closest('.date-picker').toggleClass('unselected', !this.checked);
+        var $picker = $(this).closest('.date-picker');
+        $picker.toggleClass('unselected', !this.checked);
     });
 
     $('.date-picker button, .date-picker select').on('click', function() {
@@ -44,3 +73,7 @@ $(function () {
         $(this).closest('.date-picker').removeClass('unselected');
     });
 });
+
+$.fn.smoothlyExpand = function () {
+
+};
