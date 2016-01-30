a2aead0214c3758ac5b282e894b8eb511a5c4f80
Bruno Teixeira <bteixeira666@gmail.com>
2016-01-30 15:54:54 +0100

    Added TODO. Added date picker widget for picking date range.

--- js/mobile_layout/mobile.js
+++ js/mobile_layout/mobile.js
@@ -26,4 +26,16 @@ $(function () {
         $(this).closest('.floater').removeClass('onscreen');
     });
 
+    $('.date-picker .trigger-calendar').on('click', function (ev) {
+        ev.preventDefault();
+        $(this).closest('.date-picker').find('.calendar-popup').toggleClass('onscreen');
+    });
+    $('.date-picker .calendar-popup tbody td:not(.week-number)').on('click', function (ev) {
+        $(this).closest('.date-picker').find('.trigger-calendar .value').text($(this).text());
+        $(this).closest('.date-picker').find('.calendar-popup').removeClass('onscreen');
+    });
+
+    $('.date-picker input[type="checkbox"]').on('click', function (ev) {
+        $(this).closest('.date-picker').toggleClass('unselected', !this.checked);
+    });
 });
